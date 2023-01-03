d8d48433c8df4ae12a01a241ea21aca267062e51
JHOHAN <121075923+ayhna@users.noreply.github.com>
2023-01-02 23:35:23 -0500

    Add files via upload

---
+++ trabajo.py
@@ -0,0 +1,8 @@
+nombre="Jhohan"
+ingrediente="pollo"
+estado_civil="casado"
+print(nombre)
+#concateanr variables string
+print("el nonbre es: ",nombre,"del usurario")
+print("el aji de pollo tiene como ingrediente principal:",ingrediente,)
+print("la persona {} es {}".format(nombre,estado_civil))
